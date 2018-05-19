ef7eb93c05e131818c66c8782411a699f5517294
rosstafarian <rosstafarian1@gmail.com>
2018-05-19 03:08:39 -0500

    Added REST API test for users

---
+++ tests/Feature/Api/UsersApiTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use \PassportTestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class UsersApiTest extends PassportTestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Test REST API users index
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+        $response = $this->get('/api/users');
+        $response->assertStatus(200)->assertJsonStructure(['data' => [
+                '*' => [
+                    'id',
+                    'name',
+                    'email',
+                    'admin',
+                    'created_at',
+                    'updated_at'
+                ]
+            ],
+            'status'
+        ]);
+    }
+
+    /**
+     * Test REST API users me
+     *
+     * @return void
+     */
+    public function testMe()
+    {
+        $response = $this->get('/api/me');
+        $response->assertStatus(200)->assertJsonStructure(['data' => [
+                'id',
+                'name',
+                'email',
+                'admin',
+                'created_at',
+                'updated_at'
+            ],
+            'status'
+        ]);
+    }
+
+
+
+    /**
+     * Test REST API users me
+     *
+     * @return void
+     */
+    public function testShow()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->get('/api/users/' . $user->id);
+        $response->assertStatus(200)->assertJsonStructure(['data' => [
+                'id',
+                'name',
+                'email',
+                'admin',
+                'created_at',
+                'updated_at'
+            ],
+            'status'
+        ]);
+    }
+}
