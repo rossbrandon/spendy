dd574fa8df39654911137051aa1c52b22129fbd6
rosstafarian <rosstafarian1@gmail.com>
2018-05-18 19:29:12 -0500

    Wording change for month switcher

--- resources/views/includes/monthswitcher.blade.php
+++ resources/views/includes/monthswitcher.blade.php
@@ -1,7 +1,7 @@
 <div class="row">
     <div class="col-12">
         <a href="{{ route('prev') }}" class="float-left month-switcher">
-            <span class="caret-left"></span><span class="month-switcher-text">Last Month</span>
+            <span class="caret-left"></span><span class="month-switcher-text">Previous Month</span>
         </a>
         <a href="{{ route('next') }}" class="float-right month-switcher">
             <span class="month-switcher-text">Next Month</span><span class="caret-right"></span>
